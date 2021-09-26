c00fc3cc1cd6b1a365f57176c9ca5a458b76c120
khagapati-bagh <mr.k.affinity@gmail.com>
2021-09-26 10:25:51 +0530

    added default alert

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
       <Button
         color="orange"
         title="Click Me"
-        onPress={() => console.log('Button Clicked')}
+        onPress={() => alert('Button Tapped')}
       />
     </SafeAreaView>
   );
