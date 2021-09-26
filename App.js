de2598eb82bcac0781d765346bd01e5cdededfd1
khagapati-bagh <mr.k.affinity@gmail.com>
2021-09-26 12:30:50 +0530

    added wraping items

--- App.js
+++ App.js
@@ -10,27 +10,42 @@ export default function App() {
         flexDirection: 'row', //horizontal
         justifyContent: 'center', //main axis
         alignItems: 'center', //secondary axis
+        alignContent: 'center', //only work on wrapping
+        flexWrap: 'wrap',
       }}>
       <View
         style={{
           backgroundColor: 'dodgerblue',
-          height: 100,
-          width: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'gold',
           height: 200,
           width: 100,
         }}
       />
       <View
         style={{
-          backgroundColor: 'tomato',
-          height: 300,
+          backgroundColor: 'gold',
+          height: 100,
           width: 100,
-          alignSelf: 'flex-start',
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'tomato',
+          height: 100,
+          width: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'grey',
+          height: 100,
+          width: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'greenyellow',
+          height: 100,
+          width: 100,
         }}
       />
     </View>
