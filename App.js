b47567fce6154a1b7f1617137b453e03e961831d
khagapati-bagh <mr.k.affinity@gmail.com>
2021-10-06 08:49:15 +0530

    added welcome screen

--- App.js
+++ App.js
@@ -4,5 +4,5 @@ import ViewImageScreen from './app/screen/ViewImageScreen';
 import WelcomeScreen from './app/screen/WelcomeScreen';
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <WelcomeScreen />;
 }
